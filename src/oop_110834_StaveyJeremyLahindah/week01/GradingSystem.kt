8b9698c65ddb86387ec7ca7dd8c79ecefcf4ce0f
Yunfaii <stavey.jeremy@gmail.com>
2026-02-02 15:43:11 +0700

    week 01 : Initial setup and procedural code

---
+++ src/oop_110834_StaveyJeremyLahindah/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_110834_StaveyJeremyLahindah.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score : Int = 80
+
+    println("Nama : " + name + ", Nilai: " + score)
+}
