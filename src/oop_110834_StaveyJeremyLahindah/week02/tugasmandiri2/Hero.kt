52c9d06adad62f89ba57e68672fa1a8276eb02d4
Yunfaii <stavey.jeremy@gmail.com>
2026-02-09 16:22:59 +0700

    week02: (task 2) create Hero class

--- src/oop_110834_StaveyJeremyLahindah/week02/tugasmandiri2/Hero.kt
+++ src/oop_110834_StaveyJeremyLahindah/week02/tugasmandiri2/Hero.kt
@@ -1,2 +1,21 @@
 package oop_110834_StaveyJeremyLahindah.week02.tugasmandiri2
 
+class Hero (
+    val name : String,
+    var hp : Int = 100,
+    val baseDamage : Int
+) {
+    fun attack(targetName : String) {
+        println("$name Attacks $targetName!")
+    }
+    fun takeDamage(damage : Int){
+        println("$name Takes $damage damage!")
+        hp -= damage
+        if (!isAlive()) {
+            println("$name Has fainted")
+        } else {
+            println("$name has $hp hp left!")
+        }
+    }
+    fun isAlive() : Boolean = hp > 0
+}
