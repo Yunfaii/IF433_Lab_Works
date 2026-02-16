60aebd6d0371b99e1a21fedf938d624d3fad9e9d
Yunfaii <stavey.jeremy@gmail.com>
2026-02-16 16:11:24 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_110834_StaveyJeremyLahindah/week03/tugasmandiri1/main.kt
+++ src/oop_110834_StaveyJeremyLahindah/week03/tugasmandiri1/main.kt
@@ -9,8 +9,13 @@ fun main() {
     e.damage = 2000
     e.damage = -1
     println(e.damage)
+    e.damage = 600
+    println(e.rarity)
 
     println("---Player Task---")
     val p = Player("Slick")
-
+    println("${p.username} is level ${p.level}")
+    p.addXp(50)
+    p.addXp(60)
+    println("${p.username} is level ${p.level}")
 }
