9d3966f1d6ad5dea8748af3845f87b36fb5ff7dc
Yunfaii <stavey.jeremy@gmail.com>
2026-02-16 15:27:01 +0700

    week03: test all encapsulation logic in main

--- src/oop_110834_StaveyJeremyLahindah/week03/Main.kt
+++ src/oop_110834_StaveyJeremyLahindah/week03/Main.kt
@@ -3,5 +3,11 @@ package oop_110834_StaveyJeremyLahindah.week03
 fun main () {
     val e = Employee("Budi")
     println("--- Test Error ---")
-    e.salary = 5000
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji : ${e.salary}")
+
+    e.increasePerformance()
+
+    println("Pajak yang harus dibayar : ${e.tax}")
 }
