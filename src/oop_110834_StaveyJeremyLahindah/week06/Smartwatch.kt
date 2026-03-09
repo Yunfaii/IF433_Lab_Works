da60bf833de56a7ea746ba0ae72af2ea91b8962d
Yunfaii <stavey.jeremy@gmail.com>
2026-03-09 15:11:51 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_110834_StaveyJeremyLahindah/week06/Smartwatch.kt
@@ -0,0 +1,15 @@
+package oop_110834_StaveyJeremyLahindah.week06
+
+class Smartwatch : Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala : 14:59 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("mencari perangkat HP di sekitar untuk pairing....")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
