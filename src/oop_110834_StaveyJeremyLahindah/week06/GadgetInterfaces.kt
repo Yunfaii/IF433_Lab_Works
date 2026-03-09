feb9897a98fd375dad3790e39dccc7392f0f788f
Yunfaii <stavey.jeremy@gmail.com>
2026-03-09 15:13:00 +0700

    week06: create Camera and Phone interfaces with default methods

--- src/oop_110834_StaveyJeremyLahindah/week06/GadgetInterfaces.kt
+++ src/oop_110834_StaveyJeremyLahindah/week06/GadgetInterfaces.kt
@@ -1,13 +1,13 @@
 package oop_110834_StaveyJeremyLahindah.week06
 
-//interface Camera {
-//    fun turnOn() {
-//        println ("Lensa kamera terbuka dan sensor aktif")
-//    }
-//}
-//
-//interface Phone {
-//    fun turnOn() {
-//        println("Sinyal seluler mencari jaringan")
-//    }
-//}
+interface Camera {
+    fun turnOn() {
+        println ("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan")
+    }
+}
