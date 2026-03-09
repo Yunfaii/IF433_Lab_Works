fcd4a748913a885f89d7bb92cd563902d945b9b3
Yunfaii <stavey.jeremy@gmail.com>
2026-03-09 23:25:53 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_110834_StaveyJeremyLahindah/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_110834_StaveyJeremyLahindah.week06
+
+class SmartHomeHub{
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice){
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches(){
+        for (device in devices){
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
