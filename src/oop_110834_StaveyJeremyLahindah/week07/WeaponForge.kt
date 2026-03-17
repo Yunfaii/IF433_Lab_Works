13547b7a729b45c81b7d92f9efa7d63e1e741b3c
Yunfaii <stavey.jeremy@gmail.com>
2026-03-17 09:37:56 +0700

    week07: (task) implement factory pattern in WeaponForge

--- src/oop_110834_StaveyJeremyLahindah/week07/WeaponForge.kt
+++ src/oop_110834_StaveyJeremyLahindah/week07/WeaponForge.kt
@@ -1,3 +1,14 @@
 package oop_110834_StaveyJeremyLahindah.week07
 
-class Weapon private constructor(val item: GameItem, val durability: Int)
+class Weapon private constructor(val item: GameItem, val durability: Int){
+    companion object {
+        fun forgeStarterSword(): Weapon{
+            return Weapon(GameItem("Pedang Kayu Bapuk", 5, COMMON), 50)
+        }
+
+        fun forgeEpicSword() : Weapon{
+            return Weapon(GameItem("Jovan's Halberd", 100, EPIC), 200)
+        }
+
+    }
+}
