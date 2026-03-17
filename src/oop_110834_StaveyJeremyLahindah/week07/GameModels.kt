9ddde4e94dca9e0e31ff8b02c4b2fdc3fd475124
Yunfaii <stavey.jeremy@gmail.com>
2026-03-17 09:15:38 +0700

    week07: (task) create GameItem data class

---
+++ src/oop_110834_StaveyJeremyLahindah/week07/GameModels.kt
@@ -0,0 +1,12 @@
+package oop_110834_StaveyJeremyLahindah.week07
+
+enum class ItemRarity (val dropChance : Int) {
+    COMMON (45),
+    UNCOMMON(30),
+    RARE (15),
+    EPIC (5),
+    LEGENDARY (5)
+}
+
+
+
