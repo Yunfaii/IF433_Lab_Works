33164eaf5389a7fd26736f2fac9558ea0d7ce52f
Yunfaii <stavey.jeremy@gmail.com>
2026-04-13 15:52:11 +0700

    week08: (task) create sealed class Product and data classes

--- src/oop_110834_StaveyJeremyLahindah/week08/LegacyJavaAPI.java
+++ src/oop_110834_StaveyJeremyLahindah/week08/LegacyJavaAPI.java
@@ -2,6 +2,6 @@ package oop_110834_StaveyJeremyLahindah.week08;
 
 public class LegacyJavaAPI {
     public static String fetchServerStatus () {
-        return "SERVER_ONLINE_200_OK"
+        return "SERVER_ONLINE_200_OK";
     }
 }
