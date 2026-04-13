1a29d8dec1bb7cd433c35876da96869d3733ffce
Yunfaii <stavey.jeremy@gmail.com>
2026-04-13 16:39:02 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_110834_StaveyJeremyLahindah/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_110834_StaveyJeremyLahindah.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
