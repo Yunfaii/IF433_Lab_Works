f38be8f0b2ea0a5ea15c3cd664fb537a7f25abfe
Yunfaii <stavey.jeremy@gmail.com>
2026-04-27 22:47:01 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_110834_StaveyJeremyLahindah/week10/CryptoDashboard.kt
+++ src/oop_110834_StaveyJeremyLahindah/week10/CryptoDashboard.kt
@@ -5,5 +5,9 @@ fun main() {
 
     coinRepo.add(Coin("BTC", 0.85))
     coinRepo.add(Coin("ETH", 12.4))
-    coinRepo.add(Coin("USDT", 120))
+    coinRepo.add(Coin("USDT", 120.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
+
+
 }
