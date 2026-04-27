8cf01a59fb2a7d03c0ea3bc64f4c6254c11cf06d
Yunfaii <stavey.jeremy@gmail.com>
2026-04-27 22:46:17 +0700

    week10: (task) populate coin repository

--- src/oop_110834_StaveyJeremyLahindah/week10/CryptoDashboard.kt
+++ src/oop_110834_StaveyJeremyLahindah/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_110834_StaveyJeremyLahindah.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.85))
+    coinRepo.add(Coin("ETH", 12.4))
+    coinRepo.add(Coin("USDT", 120))
 }
