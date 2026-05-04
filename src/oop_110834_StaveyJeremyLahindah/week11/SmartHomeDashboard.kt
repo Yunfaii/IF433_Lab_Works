f076b2d8e8c67006f1b919989629e606f0fe7949
Yunfaii <stavey.jeremy@gmail.com>
2026-05-04 16:10:01 +0700

    week11: (task) test full smart home configuration pipeline

--- src/oop_110834_StaveyJeremyLahindah/week11/SmartHomeDashboard.kt
+++ src/oop_110834_StaveyJeremyLahindah/week11/SmartHomeDashboard.kt
@@ -34,4 +34,8 @@ fun main() {
 
     val totalPower = homeDevices.run { sumOf { it.powerLoad } }
     println("Total Power Usage: $totalPower Watt")
+
+    homeDevices.forEach {
+        println(it.diagnose())
+    }
 }
