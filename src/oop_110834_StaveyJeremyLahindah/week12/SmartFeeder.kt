35449f2c480c2dfaac2e1e635f7fd4ed20e7c582
Yunfaii <stavey.jeremy@gmail.com>
2026-05-11 21:04:51 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_110834_StaveyJeremyLahindah/week12/SmartFeeder.kt
@@ -0,0 +1,6 @@
+package oop_110834_StaveyJeremyLahindah.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    return availableGram
+}
