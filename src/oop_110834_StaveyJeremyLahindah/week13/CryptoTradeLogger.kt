edf562f5e312fb62a9d4af17c08fae346a853772
Yunfaii <stavey.jeremy@gmail.com>
2026-05-18 15:42:03 +0700

    week13: (task) test full crypto trade logger pipeline

--- src/oop_110834_StaveyJeremyLahindah/week13/CryptoTradeLogger.kt
+++ src/oop_110834_StaveyJeremyLahindah/week13/CryptoTradeLogger.kt
@@ -56,5 +56,8 @@ fun main() {
     File("crypto_trades.csv").appendText("CORRUPT_ID, DOGEUSDT, Hold, XX, YY\n")
     val loadedData = loadTrades("crypto_trades.csv")
     val totalPnl = loadedData.sumOf { it.pnl }
+
+    loadedData.forEach { println(it) }
+    println("==== TOTAL PnL BERSIH: $totalPnl ====")
 }
 
