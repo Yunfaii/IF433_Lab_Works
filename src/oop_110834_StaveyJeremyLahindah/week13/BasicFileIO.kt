a5c69bf12d7b14572a42470a087d2cb269a99ac7
Yunfaii <stavey.jeremy@gmail.com>
2026-05-18 14:53:23 +0700

    week13: implement appendText operation

--- src/oop_110834_StaveyJeremyLahindah/week13/BasicFileIO.kt
+++ src/oop_110834_StaveyJeremyLahindah/week13/BasicFileIO.kt
@@ -5,5 +5,7 @@ fun main() {
     println("=== TEST WRITE TEXT===")
     val file = File("notes.txt")
     file.writeText("Line 1 : Inisialisasi sistem.\n")
+    file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
+    println("Teks berhasil di-append")
     println("File berhasil dibuat dan ditulis.")
 }
